3c7fd439d69b9e804b646049424934d8759eeedd
oc16252 <omar.chafik@stud.th-deg.de>
2023-07-03 21:21:27 +0200

    correlation analysis improvement

--- source_dataset/correlation_analysis.py
+++ source_dataset/correlation_analysis.py
@@ -9,17 +9,22 @@ df = pd.read_csv('source_dataset/Spotify_Youtube.csv')
 corr_matrix = df.corr()
 
 # Plot the correlation matrix as a heatmap
-sns.heatmap(corr_matrix, cmap='coolwarm')
+plt.figure(figsize=(10, 8))
+sns.heatmap(corr_matrix, cmap='coolwarm', annot=True, fmt=".2f", linewidths=0.5)
+plt.title('Correlation Matrix')
+plt.xticks(rotation=45)
+plt.yticks(rotation=0)
 plt.show()
 
 # Identify pairs of highly correlated features
 threshold = 0.8
-features_to_remove = []
+features_to_remove = set()
 for i in range(len(corr_matrix.columns)):
     for j in range(i):
         if abs(corr_matrix.iloc[i, j]) > threshold:
             print(f"{corr_matrix.columns[i]} and {corr_matrix.columns[j]} are highly correlated")
-            features_to_remove.append(corr_matrix.columns[i])
+            features_to_remove.add(corr_matrix.columns[i])
+            features_to_remove.add(corr_matrix.columns[j])
 
 # Print the list of features to remove
-print("Features to remove:", features_to_remove)
+print("Features to remove:", list(features_to_remove))
